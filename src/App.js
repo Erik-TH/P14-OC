bac34146faad23acf6f4e66b01d6fea84a7a3a66
Erik <93394265+Erik-TH@users.noreply.github.com>
2023-02-19 15:50:43 +0100

    edit path

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Header from "./components/Header";
 
 import CreateEmployee from "./pages/CreateEmployee";
-import CurrentEmployee from "./pages/CurrentEmployees";
+import CurrentEmployees from "./pages/CurrentEmployees";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route index element={<CreateEmployee />}/>
         <Route path="/createemployee" element={<CreateEmployee />} />
-        <Route path="/currentemployees" element={<CurrentEmployee />} />
+        <Route path="/currentemployees" element={<CurrentEmployees />} />
         <Route path="/*" element={<CreateEmployee />} />
       </Routes>
 
